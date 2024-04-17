19c8e64caec2ef934c1d67f8b255b3fa9316fa8a
Javialj <javialj12@gmail.com>
2024-04-17 17:00:48 +0200

    16/04/24

---
+++ src/main/java/Caja_nefra/Ejercicio01/ExcursionMetodosEstaticos.java
@@ -0,0 +1,27 @@
+package Caja_nefra.Ejercicio01;
+
+public class ExcursionMetodosEstaticos {
+    public static double calcularPrecioAlumno(int numeroAlumnos) {
+        if (numeroAlumnos >= 100) {
+            return 65;
+        } else if (numeroAlumnos >= 50 && numeroAlumnos < 100) {
+            return 70;
+        } else if (numeroAlumnos >= 30 && numeroAlumnos < 50) {
+            return 95;
+        } else {
+            return 4000/numeroAlumnos;
+        }
+    }
+
+    public static double calcularCosteExcursion(int numeroAlumnos) {
+        if (numeroAlumnos >= 100) {
+            return 65 * numeroAlumnos;
+        } else if (numeroAlumnos >= 50 && numeroAlumnos < 100) {
+            return 70 * numeroAlumnos;
+        } else if (numeroAlumnos >= 30 && numeroAlumnos < 50) {
+            return 95 * numeroAlumnos;
+        } else {
+            return 4000;
+        }
+    }
+}
